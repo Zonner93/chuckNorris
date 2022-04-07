b53f19a3198b36a7b1bcda7960f9296b60e1a749
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-07 16:43:19 +0200

    initial commit

---
+++ src/main/java/com/zonner/chuckNorris/ChuckNorrisApplication.java
@@ -0,0 +1,13 @@
+package com.zonner.chuckNorris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChuckNorrisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zonner/chuckNorris/ChuckNorrisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zonner.chuckNorris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
