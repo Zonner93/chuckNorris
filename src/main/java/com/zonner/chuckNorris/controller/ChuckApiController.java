5e1344172f78cc2f670c8227b3a62b607fee1295
Adam Pawluczuk <adam.pawluczuk@ppuc.pl>
2022-04-08 10:20:16 +0200

    fix cross origin

--- src/main/java/com/zonner/chuckNorris/controller/ChuckApiController.java
+++ src/main/java/com/zonner/chuckNorris/controller/ChuckApiController.java
@@ -2,6 +2,7 @@ package com.zonner.chuckNorris.controller;
 
 import com.zonner.chuckNorris.service.ChuckApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class ChuckApiController {
     public ChuckApiController(ChuckApiService chuckApiService) {
         this.chuckApiService = chuckApiService;
     }
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(path = "/randomjoke")
     public String getRandomJoke() {
         return chuckApiService.getRandomJoke();
